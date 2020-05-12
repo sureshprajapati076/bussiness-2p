345245cd6917977fa6da4ce54136bedaae31d6ba
Suresh Prajapati <51094657+sureshprajapati076@users.noreply.github.com>
2020-05-12 04:10:40 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { GameInitialData } from './data';
 
 declare const rollDiceWithoutValues: any;
+
+export class Player {
+  pos: number;
+  completeOneCircle: boolean
+  ownedLand: Array<Land>;
+  amount: number;
+  constructor() {
+    this.pos = 0;
+    this.ownedLand = [];
+    this.amount = 12000;
+    this.completeOneCircle = false;
+  }
+
+}
+export class Land {
+  name: String;
+  price: number;
+  ownedBy: any;
+  house: String;
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +30,237 @@ declare const rollDiceWithoutValues: any;
 })
 export class AppComponent implements OnInit {
   title = 'customjs';
-  result: any
+  result: number
+  colorGreen: String
+
+  Px: Player;
+  Py: Player;
+
+
+  P2: boolean
+  P1: boolean
+
+  data: Array<Land>
+  showHomeOption: boolean
+  showPayRent: boolean
+
+  hideBoth: boolean
+  turn: boolean;
+
   ngOnInit() {
+    this.hideBoth = false;
+
+    this.turn = true;
+    this.Px = new Player();
+    this.Py = new Player();
+
+    this.P1 = false;
+    this.P2 = true;
+    this.showHomeOption = false;
+    this.showPayRent = false;
+    this.data = GameInitialData.data;
+
 
 
   }
-  roll() {
+  buyX() {
+    let land = this.data[this.Px.pos]
+    land.ownedBy = 'red';
+
+    if (this.Px.ownedLand.indexOf(land) == -1) {
+
+      this.Px.ownedLand.push(land)
+      this.Px.amount -= land.price;
+    }
+    this.finish();
+
+  }
+
+  buyY() {
+    let land = this.data[this.Py.pos]
+    land.ownedBy = 'blue';
+    if (this.Py.ownedLand.indexOf(land) == -1) {
+      this.Py.ownedLand.push(land)
+      this.Py.amount -= land.price;
+    }
+    this.finish();
+
+
+  }
+  buildHouseX() {
+    let land = this.data[this.Px.pos]
+    if (this.Px.ownedLand.indexOf(land) >= 0) {
+      land.house = 'H';
+      this.Px.amount -= land.price * 2;
+      this.finish();
+    }
+
+
+  }
+  buildHouseY() {
+    let land = this.data[this.Py.pos]
+    if (this.Py.ownedLand.indexOf(land) >= 0) {
+      land.house = 'H';
+      this.Py.amount -= land.price * 2;
+      this.finish();
+    }
+
+  }
+  checkIfOwnedByX() {
+    let land = this.data[this.Px.pos]
+    if (this.Px.ownedLand.indexOf(land) >= 0)
+      return true;
+    else
+      return false;
+
+  }
+  checkIfOwnedByY() {
+    let land = this.data[this.Py.pos]
+    if (this.Py.ownedLand.indexOf(land) >= 0)
+      return true;
+    else
+      return false;
+
+  }
+  checkIfHouseOfX() {
+    let land = this.data[this.Px.pos]
+    if (this.Px.ownedLand.indexOf(land) >= 0 && land.house == 'H')
+      return true;
+    else
+      return false;
+
+  }
+  checkIfHouseOfY() {
+    let land = this.data[this.Py.pos]
+    if (this.Py.ownedLand.indexOf(land) >= 0 && land.house == 'H')
+      return true;
+    else
+      return false;
+
+  }
+  payRentToY() {
+    let land = this.data[this.Px.pos]
+    let payAmount = land.price;
+    if (land.house == 'H') {
+      payAmount *= 2;
+    }
+    this.Py.amount += payAmount;
+    this.Px.amount -= payAmount;
+    this.finish();
+  }
+  payRentToX() {
+    let land = this.data[this.Py.pos]
+    let payAmount = land.price;
+    if (land.house == 'H') {
+      payAmount *= 2;
+    }
+    this.Px.amount += payAmount;
+    this.Py.amount -= payAmount;
+    this.finish();
+  }
+
+  finish() {
+
+    if (this.turn == true) {
+      this.P2 = true;
+      this.P1 = false;
+    } else {
+      this.P2 = false;
+      this.P1 = true;
+    }
+    this.hideBoth = false;
+
+  }
+
+
+  roll1() {
+
+    this.result = rollDiceWithoutValues();
+    this.P1 = true;
+    this.P2 = true;
+    setTimeout(() => { this.animate1() }, 1000)
+    this.turn = false;
+
+
+
+  }
+  roll2() {
+    this.P1 = true;
+    this.P2 = true
+
     this.result = rollDiceWithoutValues();
 
+    setTimeout(() => { this.animate2() }, 1000)
+
+    this.turn = true;
+
+
+
   }
+  animate1() {
+
+    if (this.Px.pos + this.result >= 36) {
+      this.Px.completeOneCircle = true;
+    }
+    if (this.Px.completeOneCircle == false) {
+      this.finish();
+    }
+
+    for (let i = 1; i <= this.result; i++) {
+      setTimeout(() => {
+
+        this.Px.pos = (this.Px.pos + 1) % 36;
+      }, 200 * i);
+    }
+    setTimeout(() => {
+      let land = this.data[this.Px.pos];
+      this.hideBoth = true;
+      if (this.Px.ownedLand.indexOf(land) >= 0) {
+        this.showHomeOption = true;
+      } else {
+        this.showHomeOption = false;
+      }
+      if (this.Py.ownedLand.indexOf(land) >= 0) {
+        this.showPayRent = true;
+      } else {
+        this.showPayRent = false;
+      }
+
+    }, 200 * this.result)
+
+  }
+
+
+  animate2() {
+
+    if (this.Py.pos + this.result >= 36) {
+      this.Py.completeOneCircle = true;
+    }
+    if (this.Py.completeOneCircle == false) {
+      this.finish();
+    }
+    for (let i = 1; i <= this.result; i++) {
+      setTimeout(() => {
+
+        this.Py.pos = (this.Py.pos + 1) % 36;
+      }, 200 * i);
+    }
+    setTimeout(() => {
+      let land = this.data[this.Py.pos];
+      this.hideBoth = true;
+      if (this.Py.ownedLand.indexOf(land) >= 0) {
+        this.showHomeOption = true;
+      } else {
+        this.showHomeOption = false;
+      }
+      if (this.Px.ownedLand.indexOf(land) >= 0) {
+        this.showPayRent = true;
+      } else {
+        this.showPayRent = false;
+      }
+
+    }, 200 * this.result)
+
+  }
+
 }
---
+++ src/app/data.ts
@@ -0,0 +1,43 @@
+export class GameInitialData {
+    static readonly data = [
+        { 'name': 'Land0', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land1', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land2', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land3', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land4', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land5', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land6', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land7', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land8', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land9', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land10', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land11', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land12', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land13', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land14', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land15', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land16', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land17', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land18', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land19', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land20', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land21', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land22', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land23', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land24', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land25', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land26', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land27', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land28', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land29', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land30', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land31', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land32', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land33', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land34', 'price': 200, 'ownedBy': 'bank', 'house': '' },
+        { 'name': 'Land35', 'price': 200, 'ownedBy': 'bank', 'house': '' }
+    ]
+
+
+
+}
