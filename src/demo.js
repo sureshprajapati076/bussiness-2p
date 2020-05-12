e2840c92d92affe488a7c458b4f93659b2023be1
Suresh Prajapati <51094657+sureshprajapati076@users.noreply.github.com>
2020-05-11 21:31:30 -0500

    ok

---
+++ src/demo.js
@@ -0,0 +1,16 @@
+var total;
+function response(res) {
+    // returns an array of the values from the dice
+    total = res[0] + res[1];
+}
+function rollDiceWithoutValues() {
+    const element = document.getElementById('dice-box1');
+    const numberOfDice = 2;
+    const options = {
+        element, // element to display the animated dice in.
+        numberOfDice, // number of dice to use 
+        callback: response
+    }
+    rollADie(options);
+    return total;
+}
